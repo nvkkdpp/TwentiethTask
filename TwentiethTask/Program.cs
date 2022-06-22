4526aa9491d3c7552ed3544eafaee24a7371ad17
nvkkdpp <niggamemes@ya.ru>
2022-06-23 00:40:09 +0900

    commit

---
+++ TwentiethTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace TwentiethTask
+{
+   public class Student 
+    {
+     public string name;
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student student = new Student();
+            Console.WriteLine("Сначала:");
+            student.name = "Madeleine";
+            OutputName(student);
+            Console.WriteLine("После Изменения");
+            SetName(student, "Willa");
+            OutputName(student);
+            Console.WriteLine("Нажмите <enter> Для Завершения Программы . . . ");
+            Console.Read();
+        }
+        public static void OutputName(Student student)
+        {
+            Console.WriteLine("Student.name = {0}", student.name);
+        }
+        public static void SetName(Student student, string name)
+        {
+            student.name = name;
+        }
+    }
+}
